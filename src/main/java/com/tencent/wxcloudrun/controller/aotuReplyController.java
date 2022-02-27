989f5beba4ab56250d78278fdcc59e4777974f69
sdu-lifei <sdu-lifei@163.com>
2022-02-27 22:52:24 +0800

    Revert "test connect"
    This reverts commit fea8a6304fd3c15987fb96f8f453f94e413919ae.

--- src/main/java/com/tencent/wxcloudrun/controller/aotuReplyController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/aotuReplyController.java
@@ -1,12 +1,25 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.tencent.wxcloudrun.dto.WxXmlData;
 import com.tencent.wxcloudrun.service.CounterService;
+import com.tencent.wxcloudrun.service.impl.SearchServiceImpl;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.AnyTypePermission;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 自动回复消息控制器
@@ -43,53 +56,53 @@ public class aotuReplyController {
         return echostr;
     }
 
-//    @PostMapping(value = "/wx/authorize", consumes = {MediaType.TEXT_XML_VALUE}, produces = {MediaType.TEXT_XML_VALUE})
-//    public String authorize(HttpServletRequest request) {
-//        WxXmlData msg = new WxXmlData();
-//        try {
-//            msg = resolveXmlData(request.getInputStream());
-//        } catch (IOException e) {
-//            logger.error("parse msg error", e);
-//        }
-//        return autoResponse(msg);
-//    }
-//
-//    public WxXmlData resolveXmlData(InputStream in) throws IOException {
-//        StringWriter sw = new StringWriter();
-//        IOUtils.copy(in, sw, StandardCharsets.UTF_8);
-//        String xmlData = sw.toString();
-//        WxXmlData wxXmlData = null;
-//        try {
-//            XStream xstream = new XStream();
-//            xstream.addPermission(AnyTypePermission.ANY);
-//            //这个必须要加 不然无法转换成WxXmlData对象
-//            xstream.setClassLoader(WxXmlData.class.getClassLoader());
-//            xstream.processAnnotations(WxXmlData.class);
-//            xstream.alias("xml", WxXmlData.class);
-//            wxXmlData = (WxXmlData) xstream.fromXML(xmlData);
-//        } catch (Exception e) {
-//            logger.error("【error】{}", e);
-//        }
-//        return wxXmlData;
-//    }
-//
-//    public String autoResponse(WxXmlData wxData) {
-//        WxXmlData resultXmlData = new WxXmlData();
-//        resultXmlData.setToUserName(wxData.getFromUserName());  //收到的消息是谁发来的再发给谁
-//        resultXmlData.setFromUserName(wxData.getToUserName());  //
-//        resultXmlData.setMsgType("text");
-//        resultXmlData.setCreateTime(System.currentTimeMillis());
-//        String content = "Sorry, service error please try again later!";
-//        try {
-//            content = SearchServiceImpl.searchByKeyword(wxData.getContent());
-//        } catch (IOException e) {
-//            logger.error("error when try to get resource", e);
-//        }
-//
-//        resultXmlData.setContent(content);
-//        XStream xstream = new XStream();
-//        xstream.processAnnotations(WxXmlData.class);
-//        xstream.setClassLoader(WxXmlData.class.getClassLoader());
-//        return xstream.toXML(resultXmlData);
-//    }
+    @PostMapping(value = "/wx/authorize", consumes = {MediaType.TEXT_XML_VALUE}, produces = {MediaType.TEXT_XML_VALUE})
+    public String authorize(HttpServletRequest request) {
+        WxXmlData msg = new WxXmlData();
+        try {
+            msg = resolveXmlData(request.getInputStream());
+        } catch (IOException e) {
+            logger.error("parse msg error", e);
+        }
+        return autoResponse(msg);
+    }
+
+    public WxXmlData resolveXmlData(InputStream in) throws IOException {
+        StringWriter sw = new StringWriter();
+        IOUtils.copy(in, sw, StandardCharsets.UTF_8);
+        String xmlData = sw.toString();
+        WxXmlData wxXmlData = null;
+        try {
+            XStream xstream = new XStream();
+            xstream.addPermission(AnyTypePermission.ANY);
+            //这个必须要加 不然无法转换成WxXmlData对象
+            xstream.setClassLoader(WxXmlData.class.getClassLoader());
+            xstream.processAnnotations(WxXmlData.class);
+            xstream.alias("xml", WxXmlData.class);
+            wxXmlData = (WxXmlData) xstream.fromXML(xmlData);
+        } catch (Exception e) {
+            logger.error("【error】{}", e);
+        }
+        return wxXmlData;
+    }
+
+    public String autoResponse(WxXmlData wxData) {
+        WxXmlData resultXmlData = new WxXmlData();
+        resultXmlData.setToUserName(wxData.getFromUserName());  //收到的消息是谁发来的再发给谁
+        resultXmlData.setFromUserName(wxData.getToUserName());  //
+        resultXmlData.setMsgType("text");
+        resultXmlData.setCreateTime(System.currentTimeMillis());
+        String content = "Sorry, service error please try again later!";
+        try {
+            content = SearchServiceImpl.searchByKeyword(wxData.getContent());
+        } catch (IOException e) {
+            logger.error("error when try to get resource", e);
+        }
+
+        resultXmlData.setContent(content);
+        XStream xstream = new XStream();
+        xstream.processAnnotations(WxXmlData.class);
+        xstream.setClassLoader(WxXmlData.class.getClassLoader());
+        return xstream.toXML(resultXmlData);
+    }
 }
