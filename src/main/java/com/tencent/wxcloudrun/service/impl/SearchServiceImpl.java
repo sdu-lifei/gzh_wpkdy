3478804866ab850d09a3d3cdd9f4eaf08486e56c
sdu-lifei <sdu-lifei@163.com>
2022-04-17 03:08:35 +0800

    opt cache

--- src/main/java/com/tencent/wxcloudrun/service/impl/SearchServiceImpl.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/SearchServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.*;
 
@@ -21,15 +22,20 @@ import java.util.concurrent.*;
 @Slf4j
 public class SearchServiceImpl {
 
-    static int time_out = 3500;
+    static int time_out = 4000;
 
     static String def_mv = "电影仓：https://www.aliyundrive.com/s/u9BAWCEZqHi/folder/625154d1bd111a6b52bc4dc6bdb4aa8351efcffd";
     static String def_ds = "电视剧仓：https://www.aliyundrive.com/s/rJQ38Ab9Qd7/folder/6210a2fbea04dde2ea53432696ceb6ca4fbe28ec";
 
+    static String defRes = "暂未找到包含关键字的资源，看下资源库，或者稍后重试：" + System.lineSeparator() + def_mv + System.lineSeparator() + def_ds;
+
     static String base_url = "https://www.upyunso.com/";
 
-    static String start_url = "https://www.upyunso.com/search.html?page=1&search_folder_or_file=0&" +
-            "is_search_folder_content=0&is_search_path_title=0&category=video&file_extension=all&search_model=1&keyword=";
+    static String start_url = "https://www.upyunso.com/search.html?page=1&search_folder_or_file=1&is_search_folder_content=0&is_search_path_title=1&category=all&file_extension=all&search_model=1&keyword=";
+
+    static int res_limit = 10;
+
+    static String lineSp = System.lineSeparator();
 
     static ExecutorService executorService = Executors.newCachedThreadPool();
 
@@ -39,7 +45,7 @@ public class SearchServiceImpl {
             //cache最大缓存数
             .maximumSize(200)
             //设置写缓存后n秒钟过期
-            .expireAfterWrite(12, TimeUnit.HOURS)
+            .expireAfterWrite(24, TimeUnit.HOURS)
             .build();
 
     public static String searchByKeyword(String keyword) throws InterruptedException {
@@ -49,8 +55,7 @@ public class SearchServiceImpl {
 
     }
 
-    public static String getResFromWeb(String keyword) throws InterruptedException {
-        String defRes = "暂未找到包含关键字的资源，看看有没有喜欢的：" + System.lineSeparator() + def_mv + System.lineSeparator() + def_ds;
+    public static String getResFromWeb(String keyword) {
         Document document;
         try {
             document = Jsoup.parse(new URL(start_url + keyword), time_out);
@@ -58,49 +63,82 @@ public class SearchServiceImpl {
             log.error("search error", e);
             return defRes;
         }
-        Element resList = document.getElementById("res_list");
-        if (resList == null) {
+        Elements elements = document.select("div.main-info > h1 > a");
+        if (elements == null || elements.size() <= 0) {
             return defRes;
         }
-        Elements elements = resList.getElementsByAttributeValueContaining("href", "download.html");
+        // 多线程异步查找
+        executorService.submit(() -> {
+            try {
+                updateCache(keyword, elements);
+            } catch (InterruptedException e) {
+                log.error("update resource cache error", e);
+            }
+        });
 
+        // 先返回第一个
+        StringBuilder resStr = new StringBuilder("包含[ " + keyword + " ]的第一个资源：" + lineSp);
         // return top resource
-        Set<String> urlSet = new CopyOnWriteArraySet<>();
-        StringBuilder resStr = new StringBuilder("包含[ " + keyword + " ]的资源：\n");
-        CountDownLatch latch = new CountDownLatch(5);
-        for (Element element : elements) {
-            executorService.submit(() -> {
-                if (urlSet.size() < 5) {
-                    String resUrl = "";
-                    try {
-                        resUrl = getResUrl(element);
-                    } catch (IOException e) {
-                        log.error("get ali yun url error", e);
-                    }
-                    if (resUrl.length() > 0 && !urlSet.contains(resUrl) && urlSet.size() < 5) {
-                        urlSet.add(resUrl);
-                        resStr.append(element.text()).append(": ").append(resUrl).append("\n");
-                        latch.countDown();
-                    }
-                }
-            });
-            Thread.sleep(300);
-        }
-        latch.await(500, TimeUnit.MILLISECONDS);
-//        executorService.shutdown();
-        String res = urlSet.size() > 0 ? resStr.toString() : defRes;
-        resCache.put(keyword, res);
-        return res;
+        Optional<Element> element = elements.stream().filter(a -> a.attr("href").contains("download.html")).findFirst();
+        resStr.append(getResUrl(element.get())).append(lineSp).append(lineSp).append("如需更多资源请重试");
+        return resStr.toString();
     }
 
-    public static String getResUrl(Element element) throws IOException {
-        String resId = element.attributes().get("href");
-        Connection.Response response = Jsoup.connect(base_url + resId).followRedirects(false).method(Connection.Method.GET).execute();
-        return response.headers("location").size() == 0 ? "" : response.headers("location").get(0);
+    private static void updateCache(String keyword, Elements elements) throws InterruptedException {
+        log.debug("start {}", System.currentTimeMillis());
+        Set<String> urlSet = new CopyOnWriteArraySet<>();
+        Set<String> nameSet = new CopyOnWriteArraySet<>();
+        StringBuilder resStr = new StringBuilder("包含[ " + keyword + " ]的资源：" + lineSp);
+        CountDownLatch latch = new CountDownLatch(res_limit);
+        for (Element element : elements) {
+            if (!element.attr("href").contains("download.html")) {
+                continue;
+            }
+            if (urlSet.size() < res_limit) {
+                executorService.submit(() -> {
+                    String resUrl = getResUrl(element);
+                    if (resUrl.length() > 0 && !nameSet.contains(element.text()) && !urlSet.contains(resUrl) && urlSet.size() < res_limit) {
+                        urlSet.add(resUrl);
+                        nameSet.add(element.text());
+                        resStr.append(resUrl).append(lineSp);
+                        latch.countDown();
+                    }
+                });
+                Thread.sleep(400);
+            } else {
+                break;
+            }
+        }
+        log.debug("threads started {}", System.currentTimeMillis());
+        boolean done = latch.await(1000, TimeUnit.MILLISECONDS);
+        log.debug("threads end {}", done);
+        String res = urlSet.size() > 0 ? resStr.toString() : defRes;
+        log.debug("res is {}", res);
+        resCache.put(keyword, res);
+    }
+
+    public static String getResUrl(Element element) {
+        String resId = element.attr("href");
+        Connection.Response response;
+        try {
+            response = Jsoup.connect(base_url + resId).followRedirects(false).method(Connection.Method.GET).execute();
+        } catch (IOException e) {
+            log.error("get ali yun url error", e);
+            return "";
+        }
+        return response.headers("location").size() == 0 ? "" : element.text() + ": " + response.headers("location").get(0);
     }
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(searchByKeyword("山河令"));
+        Document document = null;
+        String keyword = "我的祖国";
+        try {
+            document = Jsoup.parse(new URL(start_url + keyword), time_out);
+        } catch (Exception e) {
+            log.error("search error", e);
+        }
+        assert document != null;
+        Elements resList = document.select("div.main-info > h1 > a");
     }
 
 }
